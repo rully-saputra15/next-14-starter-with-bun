04b0b47ad065e9f2f443b94182a17347427f3fb2
rully <rully.saputra4@gmail.com>
2024-04-01 22:32:34 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="font-bold text-2xl">Welcome!</h1>
+      <h1 className="font-bold text-2xl">Welcome guys!</h1>
     </main>
   );
 }
